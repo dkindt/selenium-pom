847010abc6b330ff60767777ed3eae4ee115a32a
Daniel Kindt <16381394+dkindt@users.noreply.github.com>
2020-01-28 02:03:27 -0700

    Delete result_page.py

--- result_page.py
+++
@@ -1,8 +0,0 @@
-from locators.result_page import ResultPageLocators
-
-
-class ResultPage(ResultPageLocators):
-
-    def __init__(self, browser, original_query: str = None):
-        self.browser = browser
-        self.query = original_query
